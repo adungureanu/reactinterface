625d38e36e686d56c6d03697dc6f5895e0fbb141
Ungureanu,Adrian <Adrian.Ungureanu@Cerner.com>
2020-01-16 15:32:31 +0200

    start

---
+++ src/components/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import "../css/App.css";
+
+import AddAppointments from "./AddAppointments";
+import ListAppointments from "./ListAppointments";
+import SearchAppointments from "./SearchAppointments";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      myAppointments: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("./data.json")
+      .then(response => response.json())
+      .then(result => {
+        const apts = result.map(item => {
+          return item;
+        });
+        this.setState({ myAppointments: apts });
+      });
+  }
+
+  render() {
+    return (
+      <main class="page bg-white" id="petratings">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 bg-white">
+              <div class="container">
+                <AddAppointments />
+                <SearchAppointments />
+                <ListAppointments appointments={this.state.myAppointments} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    );
+  }
+}
+
+export default App;
